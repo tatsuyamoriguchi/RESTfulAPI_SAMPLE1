e889fb8bf6c2636f9cc4ec797a3ca9a43b750c8a
Brian" Tatsuya Moriguchi <brian@beckos.com>
2020-01-06 14:55:19 -0800

    Commit 1

--- RESTfulAPI_SAMPLE1/ViewController.swift
+++ RESTfulAPI_SAMPLE1/ViewController.swift
@@ -10,11 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var restLabel: UILabel!
+    var content:String = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let urlString = "http://api.randomuser.me/"
+        let request: URLRequest = URLRequest(url: URL(string: urlString)!)
+   
+        // Without Completion handler Either one works
+        URLSession.shared.dataTask(with: request) {
+               (data: Data?, response: URLResponse?, error: Error?) in
+               
+            do {
+            
+                let json = try JSONSerialization.jsonObject(with: data!) as! NSDictionary
+                // Get the results of array and phone and email
+                if let resultsArray = json.object(forKey: "results") as? NSArray {
+                    let resultlDictionary: NSDictionary = resultsArray[0] as! NSDictionary
+                    // GEt a value from the dictionary
+                    if let email = resultlDictionary["email"] {
+                        print("email is \(email)")
+                       
+                    }
+                    if let phone = resultlDictionary["phone"] {
+                        print("phone is \(phone)")
+                        
+                    }
+                }
+                   
+            } catch {
+               print("Error")
+            }
+        }.resume()
+        
+        // With completion handler
+//        URLSession.shared.dataTask(with: request, completionHandler:  {
+//            (data: Data?, response: URLResponse?, error: Error?) in
+//            do {
+//                let json = try JSONSerialization.jsonObject(with: data!) as! NSDictionary
+//                print(json)
+//
+//                //restLabel.text = String(json)
+//
+//            } catch {
+//                print("Error")
+//            }
+//            }).resume()
+//
     }
-
-
 }
-
